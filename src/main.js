ca7b4260a7b233127c7de729a0cc8d18a9e27710
shailendra pratap singh <shailendra17singgh@gmail.com>
2018-09-21 15:40:40 +0530

    Import axios and configure axios global

--- src/main.js
+++ src/main.js
@@ -3,8 +3,19 @@ import Vue from 'vue';
 import App from '@/App.vue';
 import router from '@/router';
 import store from '@/store';
+import axios from 'axios';
 
 Vue.config.productionTip = false;
+
+const token = localStorage.getItem('user-token');
+
+axios.defaults.baseURL = 'https://api.example.com';
+axios.defaults.headers.common.Accept = 'application/json';
+axios.defaults.headers.common['Content-Type'] = 'application/json; charset=utf-8';
+
+if (token) {
+  axios.defaults.headers.common.Authorization = token;
+}
 
 new Vue({
   router,
