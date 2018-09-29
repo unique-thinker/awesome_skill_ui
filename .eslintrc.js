4ae015052d96aa4aca5a588339d05fc90ec96e58
shailendra pratap singh <shailendra17singgh@gmail.com>
2018-09-29 22:51:11 +0530

    Add no-empty-pattern eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-param-reassign': 0,
+    'no-empty-pattern': 0,
   },
   parserOptions: {
     parser: 'babel-eslint',
